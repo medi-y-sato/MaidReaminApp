6cd5940fbc21dbd427f0b44cdca0554e8e575b39
[mediba] Yoshiaki Satoh <y-sato@mediba.jp>
2017-09-26 12:02:12 +0900

    Http native (#1)
    * ionic-nativeのHTTPを無理やり使うQuickFixがあったので適用
    
    * browsertabやっぱり採用
    
    * 先方サーバに読みに行くようにしたのでREADME含めて修正
    
    * pickupでないイベントの年月指定を実装

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams, Platform } from "ionic-angular";
 
 import { InAppBrowser } from "@ionic-native/in-app-browser";
+import { BrowserTab } from "@ionic-native/browser-tab";
 
 import { EventsReaderProvider } from "../../providers/events-reader/events-reader";
 
@@ -20,30 +21,50 @@ import { EventsReaderProvider } from "../../providers/events-reader/events-reade
 export class EventsPage {
   eventList;
   pickupList;
+  targetDate;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private eventsReaderProvider: EventsReaderProvider, private inAppBrowser: InAppBrowser, private platform: Platform) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private eventsReaderProvider: EventsReaderProvider, private inAppBrowser: InAppBrowser, private platform: Platform, private browserTab: BrowserTab) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad EventsPage");
 
     this.platform.ready().then(res => {
-      this.eventsReaderProvider
-        .getEventsList()
-        .then(res => {
-          this.eventList = res;
-        })
-        .catch(error => console.error(error));
-
-      this.eventsReaderProvider
-        .getPickupList()
-        .then(res => {
-          this.pickupList = res;
-        })
-        .catch(error => console.error(error));
+      this.targetDate = new Date().toISOString();
+      this.changeDate();
     });
   }
 
+  public changeDate() {
+    const targetDateReplaced = this.targetDate.replace(/-/g, "/").substr(0, 7);
+    this.eventsReaderProvider
+      .getEventsList(targetDateReplaced)
+      .then(res => {
+        this.eventList = res;
+      })
+      .catch(error => console.error(error));
+
+    this.eventsReaderProvider
+      .getPickupList(targetDateReplaced)
+      .then(res => {
+        this.pickupList = res;
+      })
+      .catch(error => console.error(error));
+  }
+
   public link(url) {
-    this.inAppBrowser.create(url);
+    console.log("url : " + url);
+    this.browserTab
+      .isAvailable()
+      .then((isAvailable: boolean) => {
+        if (isAvailable) {
+          this.browserTab.openUrl(url);
+        } else {
+          this.inAppBrowser.create(url);
+        }
+      })
+      .catch(error => {
+        this.inAppBrowser.create(url);
+        console.error(error);
+      });
   }
 }
--- src/providers/events-reader/events-reader.ts
+++ src/providers/events-reader/events-reader.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { Http } from "@angular/http";
+import { HTTP } from "@ionic-native/http";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/toPromise";
 
@@ -12,45 +13,51 @@ import "rxjs/add/operator/toPromise";
 */
 @Injectable()
 export class EventsReaderProvider {
-  targetUrl: string = "/assets/dummy.html";
-  //  targetUrl: string = "https://maidreamin.com/event/";
+  targetUrl: string = "https://maidreamin.com/event/";
+  baseDomainUrl: string = "https://maidreamin.com";
 
-  constructor(public http: Http, private platform: Platform) {
+  constructor(public http: Http, private platform: Platform, private nativeHttp: HTTP) {
     console.log("Hello EventsReaderProvider Provider");
   }
 
-  public getPickupList(dateString?: string) {
+  public getPickupList(dateString: string) {
     return new Promise((resolve, reject) => {
       let eventList = [];
-      if (!dateString) {
-        dateString = "2017/09";
-      }
-      this.getHtml(dateString).then(res => {
-        let parser = new DOMParser();
-        let htmlData = parser.parseFromString(res.text(), "text/html");
+      console.log("dateString : " + dateString);
+      this.getHtml(dateString)
+        .then(res => {
+          let resData;
+          if (res.data) {
+            resData = res.data;
+          } else {
+            resData = res.text();
+          }
+          let parser = new DOMParser();
+          let htmlData = parser.parseFromString(resData, "text/html");
 
-        let eventsListHtml = htmlData
-          .getElementsByClassName("p-pickups")
-          .item(0)
-          .getElementsByClassName("c-column");
+          let eventsListHtml = htmlData
+            .getElementsByClassName("p-pickups")
+            .item(0)
+            .getElementsByClassName("c-column");
 
-        for (let i = 0; i < eventsListHtml.length; i++) {
-          let url = eventsListHtml.item(i).getElementsByTagName("a")[0].href;
-          url = url.replace(/http:\/\/(.*?)\//, "https://maidreamin.com/");
-          const eventData = {
-            link: url,
-            thumbnail: eventsListHtml
-              .item(i)
-              .getElementsByClassName("c-thumb")[0]
-              ["style"].backgroundImage.match(/url\("(.*)"\)/)[1],
-            date: eventsListHtml.item(i).getElementsByClassName("c-thumb__date")[0]["innerText"],
-            title: eventsListHtml.item(i).getElementsByClassName("p-pickup__ttl")[0]["innerText"],
-            ex: eventsListHtml.item(i).getElementsByClassName("p-pickup__ex")[0]["innerText"]
-          };
-          eventList.push(eventData);
-        }
-      });
-      resolve(eventList);
+          for (let i = 0; i < eventsListHtml.length; i++) {
+            const eventData = {
+              link: this.baseDomainUrl + eventsListHtml.item(i).getElementsByTagName("a")[0].attributes[0].value,
+              thumbnail: eventsListHtml
+                .item(i)
+                .getElementsByClassName("c-thumb")[0]
+                ["style"].backgroundImage.match(/url\("(.*)"\)/)[1],
+              date: eventsListHtml.item(i).getElementsByClassName("c-thumb__date")[0]["innerText"],
+              title: eventsListHtml.item(i).getElementsByClassName("p-pickup__ttl")[0]["innerText"],
+              ex: eventsListHtml.item(i).getElementsByClassName("p-pickup__ex")[0]["innerText"]
+            };
+            eventList.push(eventData);
+          }
+          resolve(eventList);
+        })
+        .catch(error => {
+          reject(error);
+        });
     });
   }
 
@@ -60,35 +67,47 @@ export class EventsReaderProvider {
       if (!dateString) {
         dateString = "2017/09";
       }
-      this.getHtml(dateString).then(res => {
-        let parser = new DOMParser();
-        let htmlData = parser.parseFromString(res.text(), "text/html");
+      this.getHtml(dateString)
+        .then(res => {
+          let resData;
+          if (res.data) {
+            resData = res.data;
+          } else {
+            resData = res.text();
+          }
+          let parser = new DOMParser();
+          let htmlData = parser.parseFromString(resData, "text/html");
 
-        let eventsListHtml = htmlData
-          .getElementsByClassName("p-events-sec__list")
-          .item(0)
-          .getElementsByClassName("p-event");
+          let eventsListHtml = htmlData
+            .getElementsByClassName("p-events-sec__list")
+            .item(0)
+            .getElementsByClassName("p-event");
 
-        for (let i = 0; i < eventsListHtml.length; i++) {
-          let url = eventsListHtml.item(i).getElementsByTagName("a")[0].href;
-          url = url.replace(/http:\/\/(.*?)\//, "https://maidreamin.com/");
-          const eventData = {
-            url: url,
-            time: eventsListHtml
-              .item(i)
-              .getElementsByClassName("p-event__time")
-              .item(0)["innerText"],
-            shopname: eventsListHtml.item(i).getElementsByClassName("p-event__shop-name")[0]["innerText"],
-            title: eventsListHtml.item(i).getElementsByClassName("p-event__title")[0]["innerText"]
-          };
-          eventList.push(eventData);
-        }
-      });
-      resolve(eventList);
+          for (let i = 0; i < eventsListHtml.length; i++) {
+            const eventData = {
+              url: this.baseDomainUrl + eventsListHtml.item(i).getElementsByTagName("a")[0].attributes[0].value,
+              time: eventsListHtml
+                .item(i)
+                .getElementsByClassName("p-event__time")
+                .item(0)["innerText"],
+              shopname: eventsListHtml.item(i).getElementsByClassName("p-event__shop-name")[0]["innerText"],
+              title: eventsListHtml.item(i).getElementsByClassName("p-event__title")[0]["innerText"]
+            };
+            eventList.push(eventData);
+          }
+          resolve(eventList);
+        })
+        .catch(error => reject(error));
     });
   }
 
   private getHtml(dateString) {
-    return this.http.get(this.targetUrl + "?date=" + dateString).toPromise();
+    let returnObject;
+    if (this.platform.is("cordova")) {
+      returnObject = this.nativeHttp.get(this.targetUrl + "?date=" + dateString, {}, {});
+    } else {
+      returnObject = this.http.get(this.targetUrl + "?date=" + dateString).toPromise();
+    }
+    return returnObject;
   }
 }
