1b25da82d254b7877e05df8ac2b395a9ac76c0ba
Yoshiaki Satoh <y-sato@mediba.jp>
2017-09-26 09:58:37 +0900

    ionic-nativeのHTTPを無理やり使うQuickFixがあったので適用

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,4 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+// quick fix for reference problem
+// https://github.com/ionic-team/ionic-native/pull/1934#issuecomment-331861871
+import { HTTP } from '@ionic-native/http';
+HTTP.getPluginRef = () => "cordova.plugin.http";
+
 platformBrowserDynamic().bootstrapModule(AppModule);
